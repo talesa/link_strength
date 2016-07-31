corrected P calculation to my new understanding

link_strength.py
@@ -1,17 +1,17 @@
 
 # coding: utf-8
 
-# In[89]:
+# In[329]:
 
 # define variables
 
-number_of_conditioned_variables = 2
+number_of_conditioned_variables = 5
 number_of_states_in_variable = 3
 
 
-# In[90]:
+# In[330]:
 
-# 
+# magic
 
 number_of_states_in_conditioned_variable = []
 for i in range(number_of_conditioned_variables):
@@ -22,23 +22,24 @@ for i in range(number_of_conditioned_variables):
     link_strength.append(0)
 
 
-# In[115]:
+# In[331]:
 
 # define variables
 
 number_of_states_in_conditioned_variable[0] = 3
 number_of_states_in_conditioned_variable[1] = 3
+number_of_states_in_conditioned_variable[2] = 3
+number_of_states_in_conditioned_variable[3] = 3
+number_of_states_in_conditioned_variable[4] = 3
 
-link_strength[0] = 0.5
-link_strength[1] = 0.5
+link_strength[0] = 0.99
+link_strength[1] = 0.6
+link_strength[2] = 0.6
+link_strength[3] = 0.6
+link_strength[4] = 0.6
 
 
-# In[116]:
-
-number_of_states_in_conditioned_variable
-
-
-# In[117]:
+# In[332]:
 
 # create placeholders for P(u'|u)
 
@@ -50,13 +51,14 @@ for i in range(number_of_conditioned_variables):
     P.append(np.matrix(np.zeros((number_of_states_in_conditioned_variable[i], number_of_states_in_conditioned_variable[i]))))
 
 
-# In[118]:
+# In[333]:
 
-for state_of_variable in range(number_of_states_in_variable):
-    for conditioned_variable_no in range(number_of_conditioned_variables):
-        for state_of_conditioned_variable in range(number_of_states_in_conditioned_variable[conditioned_variable_no]):
-            r = state_of_variable
-            c = state_of_conditioned_variable
+# for state_of_variable in range(number_of_states_in_variable):
+for conditioned_variable_no in range(number_of_conditioned_variables):
+    for r in range(number_of_states_in_conditioned_variable[conditioned_variable_no]):
+        for c in range(number_of_states_in_conditioned_variable[conditioned_variable_no]):
+#             r = state_of_variable
+#             c = state_of_conditioned_variable
             m_i = float(number_of_states_in_conditioned_variable[conditioned_variable_no])
             abs_eta_i = np.abs(link_strength[conditioned_variable_no])
             K = 1 - 1/m_i
@@ -70,7 +72,12 @@ for state_of_variable in range(number_of_states_in_variable):
                 P[conditioned_variable_no][r, c] = (abs_eta_i/(c-r)**2/sum_term + (1-abs_eta_i)/(m_i-1))*(1-1/m_i-abs_eta_i*K)
 
 
-# In[119]:
+# In[334]:
+
+P
+
+
+# In[335]:
 
 def F(u_prime):
     res = sum([np.abs(eta_i)*H(u_prime_i, i) for i, (eta_i, u_prime_i) in enumerate(zip(link_strength, u_prime))])
@@ -79,7 +86,7 @@ def F(u_prime):
     return res
 
 
-# In[120]:
+# In[336]:
 
 def H(u_prime_i, i):
     if link_strength[i] >= 0:
@@ -88,7 +95,7 @@ def H(u_prime_i, i):
         return -u_prime_i + 1 + number_of_states_in_conditioned_variable[i]
 
 
-# In[121]:
+# In[337]:
 
 import itertools
 
@@ -102,17 +109,12 @@ def generate_all_possible_u():
     return list(itertools.product(*groups))
 
 
-# In[122]:
-
-P
-
-
-# In[123]:
+# In[338]:
 
 b = [1.5, 0.6, 1.5]
 
 
-# In[157]:
+# In[339]:
 
 # P(x|u)
 # = sum over all u' such that F(u') = x
@@ -146,14 +148,14 @@ def Pr(x, u):
             prod_term *= P[i][u[i], u_prime[i]]
 
     
-        print (correction(weight, x))
+#         print (correction(weight, x))
         
         sum_term += prod_term #+ correction(weight, x)
         
     return sum_term #+ correction(weight, x)
 
 
-# In[158]:
+# In[340]:
 
 def correction(weight2, x):
     c1 = np.max(np.abs(link_strength)) * weight2 + (1 - np.max(np.abs(link_strength))) * b[x] * weight2
@@ -165,22 +167,13 @@ def correction(weight2, x):
     return c2
 
 
-# In[159]:
+# In[341]:
 
 for us in generate_all_possible_u():
     l = [Pr(i, us) for i in range(number_of_states_in_variable)]
     suml = sum(l)
-#     if suml != 0:
-#         l = [i/suml for i in l]
-    print (l)
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
+    if suml != 0:
+        l = [i/suml for i in l]
+    l = ["%.2f" % i for i in l]
+    print ('{}: {}'.format(us, l))
 